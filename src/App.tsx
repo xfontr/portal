df15de3e003cda7e19a2320d1e7f75b57b481efa
Xifre Font <fontx@es-embp2022.es.lidl.net>
2022-11-16 16:38:18 +0100

    Set snowball and trees background with animations

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-const App = () => (
-  <div className="app">
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
-  </div>
-);
-
-export default App;
---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+const App = () => (
+  <div className="app">
+    <div className="snowball">
+      <h1>ENJOY CHRISTMAS HOLIDAYS</h1>
+    </div>
+  </div>
+);
+
+export default App;
