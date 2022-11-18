140cf35627490faad8275dfacd801e2548b61d22
Xifre Font <fontx@es-embp2022.es.lidl.net>
2022-11-18 09:37:24 +0100

    Abstract the counter logic to a custom hook

--- src/features/counter/components/DateCounter.tsx
+++ src/features/counter/components/DateCounter.tsx
@@ -1,11 +1,11 @@
 import moment from "moment";
 import { useEffect, useState } from "react";
+import useCounter from "../hooks/useCounter";
 import isDateZero from "../services/isDateZero";
 import CountData from "../types/CountData";
 import Counter from "./Counter/Counter";
 import CounterFinish from "./CounterFinish";
 
-const second = 1000;
 const currentTime = moment();
 
 type DateCounterProps = {
@@ -23,39 +23,7 @@ const DateCounter = ({ limitDate }: DateCounterProps): JSX.Element => {
     seconds: duration.seconds(),
   });
 
-  useEffect(() => {
-    const eventTime = moment(limitDate);
-    let duration = moment.duration(eventTime.diff(currentTime));
-
-    const counter = setInterval(() => {
-      duration = moment.duration(
-        duration.asMilliseconds() - second,
-        "milliseconds"
-      );
-
-      const date = {} as CountData;
-
-      Math.floor(duration.asDays()) >= 0
-        ? (date.days = Math.floor(duration.asDays()))
-        : (date.days = 0);
-
-      duration.hours() >= 0
-        ? (date.hours = duration.hours())
-        : (date.hours = 0);
-
-      duration.minutes() >= 0
-        ? (date.minutes = duration.minutes())
-        : (date.minutes = 0);
-
-      duration.seconds() >= 0
-        ? (date.seconds = duration.seconds())
-        : (date.seconds = 0);
-
-      setViewDate(date);
-    }, second);
-
-    return () => clearInterval(counter);
-  }, [limitDate]);
+  useCounter(setViewDate, limitDate, currentTime);
 
   return (
     <>
